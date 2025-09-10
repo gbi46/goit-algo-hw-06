34d19e876196ccb6c832ce206ebe817956ea80da
ivcoder <vank300828@gmail.com>
2025-09-10 19:57:23 +0200

    add knut-morris-prat function

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -22,3 +22,34 @@ def boyer_moore(text: str, pattern: str) -> int:
             return i
         i += skip.get(text[i + m - 1], m)
     return -1
+
+def kmp(text: str, pattern: str) -> int:
+    n, m = len(text), len(pattern)
+    if m == 0:
+        return 0
+    lps = [0] * m
+    length = 0
+    i = 1
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+    i = j = 0
+    while i < n:
+        if pattern[j] == text[i]:
+            i += 1; j += 1
+            if j == m:
+                return i - j
+        else:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return -1
